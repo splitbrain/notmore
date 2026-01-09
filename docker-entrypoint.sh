d06fb6d2f4843fb608bba0e4149a8e11c7912530
Andreas Gohr <andi@splitbrain.org>
2026-01-09 15:31:20 +0100

    do not try to modify the volumes and config location
    install tries to modify the permissions - we can't do that when running
    rootless.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,9 @@ set -e
 : "${NOTMUCH_PRIMARY_EMAIL:?Set NOTMUCH_PRIMARY_EMAIL}"
 : "${NOTMUCH_OTHER_EMAILS:=}"
 
-# Ensure the target directory exists for the generated config
-install -d "$(dirname "$NOTMUCH_CONFIG")"
-
 # Export the config path and generate the config file
 CONFIG_TEMPLATE=/app/conf/notmuch-config.template
 envsubst < "$CONFIG_TEMPLATE" > "$NOTMUCH_CONFIG"
-
-# Ensure the database and mail roots exist before initializing notmuch
-install -d /mail /notmuch
 
 # Initialize or update the notmuch database when starting
 notmuch new
